e96eda73c92861f108e3fbcc3ce3e1940ef7d555
imfarhad <fakhrul.farhad@gmail.com>
2020-09-19 07:39:01 +0300

    added double click handler in todo item

--- src/FirebaseTodoCollectionTest.jsx
+++ src/FirebaseTodoCollectionTest.jsx
@@ -3,11 +3,18 @@ import { useFirestore, useFirestoreCollectionData } from "reactfire";
 
 const FirebaseTodoCollectionTest = () => {
   const todoCollectionRef = useFirestore().collection("todos");
-  const todos = useFirestoreCollectionData(todoCollectionRef);
+  const dbtodos = useFirestoreCollectionData(todoCollectionRef);
+
+  const onItemDoubleClick = (name) => {
+    console.log(name + "-- to be deleted");
+  };
+
   return (
     <ul>
-      {todos.map((item, id) => (
-        <li key={id}>{item.name}</li>
+      {dbtodos.map((item, id) => (
+        <li key={id} onDoubleClick={() => onItemDoubleClick(item.name)}>
+          {item.name}
+        </li>
       ))}
     </ul>
   );
